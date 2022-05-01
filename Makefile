c2b399c79a2258f797ac18863c28d89a258e1123
Vincent Clemson <vclemson07@gmail.com>
2022-05-01 02:55:32 -0400

    fix chunk name section anchor knit hook

--- Makefile
+++ Makefile
@@ -2,11 +2,14 @@
 
 all: build
 
-build: | docs/libs
+build: | docs/libs docs/.nojekyll
 	Rscript build/build_pca_mlr3_pipelines.R
 
 docs/libs:
 	mkdir -p docs/libs
+	
+docs/.nojekyll:
+	touch docs/.nojekyll
 	
 preview:
 	Rscript utils/preview.R
--- build/build_pca_mlr3_pipelines.R
+++ build/build_pca_mlr3_pipelines.R
@@ -2,6 +2,7 @@ library(tictoc)
 
 tic()
 rmarkdown::render(
-  input = "pca-mlr3-pipelines.Rmd", output_dir = "docs", output_format = "html_document"
+  input = "pca-mlr3-pipelines.Rmd", output_format = "html_document",
+  output_file = "docs/index.html"
 )
 toc()
